50a8bf7b33235c79e0bd3d6a8d370e1c74e8d3ee
nchallapalli <nchallapalli@opentable.com>
2013-09-12 00:45:12 -0700

    Adding Google GeoCoding service to KitchenSink. This service takes care of using google geocoder to geo code and bring back valid lat/lng and formatted addresss.

---
+++ src/services/googleGeoCoding.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+angular.module("KitchenSink").factory("googleGeoCoding", ["$window", "$timeout", "$q", function ($window, $timeout, $q) {
+	return {
+		GeoCodeAddress: function (address, region) {
+		
+			var defer = $q.defer();
+
+			if (!$window.google) {
+				$timeout(function() {
+					 defer.reject({ code: -1, message: "Google Geocoding service is unavailable, please make sure Api libraries are referenced correctly." });;
+				}, 0);
+				return defer.promise;
+			}			
+
+			if (!address || !region) {
+				$timeout(function() {
+					defer.reject({code: -1, message: "Invalid arguments, Please provide valid address and region code(ex: US, GB etc)" });
+				}, 0);
+				return defer.promise;
+			}
+
+			$timeout(function() {
+				var googleClient = new $window.google.maps.Geocoder();
+				googleClient.geocode({ 'address': address, 'region': region },
+					function(results, status) {
+						if (status == $window.google.maps.GeocoderStatus.OK) {
+							defer.resolve(results);
+						} else {
+							defer.reject({ code: status, message: "Unable to locate address at this time." });
+						}
+					});
+			}, 0);
+
+			return defer.promise;
+		}
+	};
+}]);
---
+++ test/unit/services/googleGeoCoding.spec.js
@@ -0,0 +1,110 @@
+﻿describe("KitchenSink GoogleGeoCoding", function() {
+	var geoCodingService = null,
+		googleGeoCoderApi = null,
+		onSuccesCallback = null,
+		onErrorCallback = null,
+		status = null,
+		results = null,
+		error = null;
+		
+
+	var $window = null,
+		$timeout = null;
+	
+	beforeEach (function() {
+		status = "OK"; 
+
+		//mock google library object
+		$window = {
+			google: {
+				maps: {
+					GeocoderStatus: {OK: "OK", ERROR:"ERROR"},
+					Geocoder:function () {
+						return {
+							geocode: jasmine.createSpy().andCallFake(function(params) {
+								results = [{
+									formatted_address: "San Francisco CA, USA",
+									geometry: {
+										location: {
+											lat: function() {
+												return 37.7749295;
+											},
+											lng: function() {
+												return -122.41941550000001;
+											}
+										}
+									}
+								}];
+								arguments[1](results, status);
+							})
+						};
+					}
+				}
+			},
+			navigator: {
+				userAgent:""
+			}
+		};
+
+		module("KitchenSink");
+			
+		module(function($provide) {
+			$provide.value("$window", $window);
+		});
+
+		inject(function($injector) {
+			geoCodingService = $injector.get("googleGeoCoding");
+			$timeout = $injector.get("$timeout");
+		});
+
+		onSuccesCallback = jasmine.createSpy();
+		onErrorCallback = jasmine.createSpy();
+	});
+
+	describe("test geoCoding with success callback", function () {
+		it("should process address and return valid geo results through callback", function () {
+			makegeoCodingCall("sf", "US");
+			expect(onSuccesCallback).toHaveBeenCalledWith(results);
+		});
+	});
+	
+	describe("test error while geocoding with error callback", function () {
+		beforeEach(function () {
+			status = "ERROR";
+			error = { code: status, message: "Unable to locate address at this time." };
+		});
+	
+		it("should send error status with message", function () {
+			makegeoCodingCall("sf", "US");
+			expect(onErrorCallback).toHaveBeenCalledWith(error);
+		});
+	});
+
+	describe("test error when invalid arguments are passed in", function () {
+		beforeEach(function () {
+			error = { code: -1, message: "Invalid arguments, Please provide valid address and region code(ex: US, GB etc)" };
+		});
+
+		it("should raise error with invalid arguments message", function () {
+			makegeoCodingCall("sf", null);
+			expect(onErrorCallback).toHaveBeenCalledWith(error);
+		});
+	});
+			
+	describe("test error when google geo coding service is unavailable", function () {
+		beforeEach(function () {
+			error = { code: -1, message: "Google Geocoding service is unavailable, please make sure Api libraries are referenced correctly." };
+			$window.google = null;
+			makegeoCodingCall("sf", "US");
+		});
+
+		it("should raise error with google geocoder service unavailable message", function () {
+			expect(onErrorCallback).toHaveBeenCalledWith(error);
+		});
+	});
+
+	function makegeoCodingCall(address, region) {
+		geoCodingService.GeoCodeAddress(address, region).then(onSuccesCallback, onErrorCallback);
+		$timeout.flush();
+	}
+});
